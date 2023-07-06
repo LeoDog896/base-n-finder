9e11bce710b7c368cae50904508d9f57b029f5e3
Tristan <LeoDog896@hotmail.com>
2023-07-06 19:00:33 -0400

    feat: arbitrary prefix

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,17 @@ fn main() {
     let mut start = BigUint::from(2u32);
     let mut count = 0;
 
+    let prefix = BigUint::from(8675309u32);
+    let prefix_str = prefix.to_string();
+
     loop {
-        if start.to_string().starts_with("8675309") {
-            println!("2^{} starts with 8675309", count);
+        if start.to_string().starts_with(&prefix_str) {
+            println!("2^{} starts with {}", count, prefix_str);
             break;
         }
 
         println!("2^{}", count);
-        start = start.clone() * start;
+        start = start.pow(2);
         count += 1;
     }
 }
