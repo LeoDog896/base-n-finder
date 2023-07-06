dc0afdb63404f241de7692cca23f7968fd2598f4
Tristan <LeoDog896@hotmail.com>
2023-07-06 19:21:03 -0400

    feat: lock stdout

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use rug::{Integer, ops::Pow};
+use std::io::Write;
 
 fn main() {
     let mut count = 0;
+
+    let stdout = std::io::stdout();
+    let mut lock = stdout.lock();
 
     let prefix = Integer::from(8_675_309u32);
     let prefix_str = prefix.to_string();
@@ -9,11 +13,11 @@ fn main() {
     loop {
         let start = Integer::from(2).pow(count);
         if start.to_string().starts_with(&prefix_str) {
-            println!("2^{count} starts with {prefix_str}");
+            let _ = writeln!(lock, "2^{count} starts with {prefix_str}");
             break;
         }
 
-        println!("2^{count}");
+        let _ = writeln!(lock, "2^{count}");
         count += 1;
     }
 }
