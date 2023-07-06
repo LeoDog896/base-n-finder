192b0c6347a087583ea96978bee767121d11aeab
Tristan <LeoDog896@hotmail.com>
2023-07-06 18:55:51 -0400

    feat: base simulation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use num_bigint::BigUint;
+
 fn main() {
-    println!("Hello, world!");
+    let mut start = BigUint::from(2u32);
+    let mut count = 0;
+
+    loop {
+        if start.to_string().starts_with("8675309") {
+            println!("2^{} starts with 8675309", count);
+            break;
+        }
+
+        println!("2^{}", count);
+        start = start.clone() * start;
+        count += 1;
+    }
 }
