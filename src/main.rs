fe503dff561aabb77a0e94df7f26b0ea6ff100c9
Tristan <LeoDog896@hotmail.com>
2023-07-06 23:07:48 -0400

    style: fmt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use rug::{Integer, ops::Pow};
-use std::{io::Write, env::args};
+use rug::{ops::Pow, Integer};
+use std::{env::args, io::Write};
 
+// highest so far has been 2^1326694
 fn main() {
-    let mut count = args().nth(1).unwrap_or("0".to_string()).parse::<u32>().unwrap();
+    let mut count = args()
+        .nth(1)
+        .unwrap_or_else(|| "0".to_string())
+        .parse::<u32>()
+        .unwrap();
     let mut start = Integer::from(2).pow(count);
 
     let stdout = std::io::stdout();
@@ -12,7 +17,7 @@ fn main() {
     let prefix_str = prefix.to_string();
 
     loop {
-        start = start * 2;
+        start *= 2;
         let string = start.to_string_radix(10);
         if string.starts_with(&prefix_str) {
             let _ = writeln!(lock, "2^{count} starts with {prefix_str}");
